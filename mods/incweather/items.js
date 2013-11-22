eca4d6fa4db4e264e14a3be37ddd9a8c0a515652
ChampionOnyx <shisien_0928@live.com>
2013-11-22 21:55:54 +0800

    Update items.js

--- mods/incweather/items.js
+++ mods/incweather/items.js
@@ -1,12 +1,13 @@
 exports.BattleItems = {
         "molarrock": {
-		            id: "molarrock",
-		            name: "Molar Rock",
-		            spritenum: 0,
-		            fling: {
-			                  basePower: 10
-		            },
-		            num: 1000,
-		            gen: 0,
-		            desc: "Holder's use of Acid Rain lasts 8 turns instead of 5."
-	       },
+                id: "molarrock",
+		name: "Molar Rock",
+		spritenum: 0,
+		fling: {
+			basePower: 10
+	        },
+		num: 1000,
+		gen: 0,
+		desc: "Holder's use of Acid Rain lasts 8 turns instead of 5."
+	}
+};
