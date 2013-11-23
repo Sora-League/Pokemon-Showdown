06dd3b309b863ae555b6d0ee99d65a7aa7c3acee
ChampionOnyx <shisien_0928@live.com>
2013-11-23 19:56:37 +0800

    Update pokedex.js

--- mods/incweather/pokedex.js
+++ mods/incweather/pokedex.js
@@ -1,4 +1,4 @@
 exports.BattlePokedex = {
-koffing:{num:109,species:"Koffing",types:["Poison"],baseStats:{hp:40,atk:65,def:95,spa:60,spd:45,spe:35},abilities:{0:"Levitate,1:"Contamination"},heightm:0.6,weightkg:1,color:"Purple",evos:["weezing"],eggGroups:["Amorphous"]},
+koffing:{num:109,species:"Koffing",types:["Poison"],baseStats:{hp:40,atk:65,def:95,spa:60,spd:45,spe:35},abilities:{0:"Levitate",1:"Contamination"},heightm:0.6,weightkg:1,color:"Purple",evos:["weezing"],eggGroups:["Amorphous"]},
 weezing:{num:110,species:"Weezing",types:["Poison"],baseStats:{hp:65,atk:90,def:120,spa:85,spd:70,spe:60},abilities:{0:"Levitate",1:"Contamination"},heightm:1.2,weightkg:9.5,color:"Purple",prevo:"koffing",evoLevel:35,eggGroups:["Amorphous"]}
 };
