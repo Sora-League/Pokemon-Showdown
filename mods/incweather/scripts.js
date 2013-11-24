8cef33096768cba9720cd58ac07f51d4e25134f9
ChampionOnyx <shisien_0928@live.com>
2013-11-24 20:53:16 +0800

    Create scripts.js

---
+++ mods/incweather/scripts.js
@@ -0,0 +1,23 @@
+exports.BattleScripts = {
+        inherit: 'gen5',
+        gen: 5,
+        init: function() {
+                //Pokemon that can learn Contaminate
+                this.modData('Learnsets', 'bulbasaur').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'ivysaur').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'venusaur').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'ekans').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'arbok').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'oddish').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'gloom').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'vileplume').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'bellsprout').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'weepinbell').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'victreebell').learnset.contaminate = ['5M'];
+                this.modData('Learnsets', 'weezing').learnset.contaminate = ['5M'];
+                
+                //Weezing and Koffing has Contaminate
+                this.modData('Pokedex', 'koffing').abilities['1'] = 'Contamination';
+                this.modData('Pokedex', 'weezing').abilities['1'] = 'Contamination';
+        }
+};
