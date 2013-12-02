9d23bd2f6737de7609352300e823d0f530caf94d
ChampionOnyx <shisien_0928@live.com>
2013-12-02 14:01:18 +0800

    Update moves.js

--- mods/incweather/moves.js
+++ mods/incweather/moves.js
@@ -6,7 +6,7 @@ exports.BattleMovedex = {
 		      category: "Status",
 		      desc: "For 5 turns, the weather becomes Acid Rain. At the end of each turn except the last, all active Pokemon lose 1/16 of their maximum HP, rounded down, unless they are a Poison or Steel-type, or have the Abilities Magic Guard or Overcoat. The Defense of Poison-types is 1.5x and the power of Ground-type attacks is 0.5x during the effect. Lasts for 8 turns if the user is holding Molar Rock. Fails if the current weather is Acid Rain.",
 		      shortDesc: "For 5 turns, corroding Acid Rain falls.",
-		      id: "contaminate",
+		      id: "Contaminate",
 		      name: "Contaminate",
 		      pp: 10,
 		      priority: 0,
