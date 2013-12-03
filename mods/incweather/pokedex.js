336dd5e919e4301465a821ce5ec5938ed75d0943
ChampionOnyx <shisien_0928@live.com>
2013-12-03 18:29:52 +0800

    Update pokedex.js

--- mods/incweather/pokedex.js
+++ mods/incweather/pokedex.js
@@ -1,4 +1,5 @@
 exports.BattlePokedex = {
+beedrill:{num:15,species:"Beedrill",types:["Bug","Poison"],baseStats:{hp:65,atk:90,def:40,spa:45,spd:80,spe:75},abilities:{0:"Swarm",1:"Acid Rush",H:"Sniper"},heightm:1,weightkg:29.5,color:"Yellow",prevo:"kakuna",evoLevel:10,eggGroups:["Bug"]},
 koffing:{num:109,species:"Koffing",types:["Poison"],baseStats:{hp:40,atk:65,def:95,spa:60,spd:45,spe:35},abilities:{0:"Levitate",1:"Contamination"},heightm:0.6,weightkg:1,color:"Purple",evos:["weezing"],eggGroups:["Amorphous"]},
 weezing:{num:110,species:"Weezing",types:["Poison"],baseStats:{hp:65,atk:90,def:120,spa:85,spd:70,spe:60},abilities:{0:"Levitate",1:"Contamination"},heightm:1.2,weightkg:9.5,color:"Purple",prevo:"koffing",evoLevel:35,eggGroups:["Amorphous"]}
 };
