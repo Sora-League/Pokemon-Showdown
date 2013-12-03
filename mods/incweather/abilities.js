ec1f9f7a1a2f35480b9a7517cfd11b23b6070d6a
ChampionOnyx <shisien_0928@live.com>
2013-12-03 18:22:35 +0800

    Update abilities.js

--- mods/incweather/abilities.js
+++ mods/incweather/abilities.js
@@ -9,5 +9,21 @@ exports.BattleAbilities = {
 		name: "Contamination",
 		rating: 4.5,
 		num: 1000
+	},
+	"acidrush": {
+		desc: "This Pokemon's Speed is doubled if the weather is Acid Rain. This Pokemon is also immune to residual Acid Rain damage.",
+		shortDesc: "If Acid Rain is active, this Pokemon's Speed is doubled; immunity to Acid Rain.",
+		onModifySpe: function(speMod, pokemon) {
+			if (this.isWeather('acidrain')) {
+				return this.chain(speMod, 2);
+			}
+		},
+		onImmunity: function(type, pokemon) {
+			if (type === 'acidrain') return false;
+		},
+		id: "acidrush",
+		name: "Acid Rush",
+		rating: 2,
+		num: 1001
 	}
 };
