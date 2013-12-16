63ebc276b9e73e80f0789b54e1f8324373fc20a1
ChampionOnyx <shisien_0928@live.com>
2013-12-16 18:17:32 +0800

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -954,13 +954,12 @@ var commands = exports.commands = {
 			  '<img src="http://i.imgur.com/zVptN2v.png"> <img src="http://play.pokemonshowdown.com/sprites/trainers/9.png">');
         },
         
-        sjado: function(target, room, user) {
+        hellfire: function(target, room, user) {
 		    if (!this.canBroadcast()) return;
-			this.sendReplyBox('Gym Ldr <b>Sjado</b><br />'+
-			  '<i>"No Pain, No Game."</i> <br />'+
+			this.sendReplyBox('Gym Ldr <b>HellFire</b><br />'+
+			  '<i>"Welcome to the infinite darkness."</i> <br />'+
 			  '<b>Type:</b> Dark<br />'+
-			  '<b>Ace:</b> Hydreigon<br />'+
-			  '<img src="http://sprites.pokecheck.org/i/635.gif"> <img src="http://sprites.pokecheck.org/t/035.gif">');
+			  '<b>Ace:</b> Umbreon<br />');
         },
         
         phantoms: function(target, room, user) {
