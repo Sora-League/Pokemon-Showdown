dce43f376ab35fb8688daa7de4fcbb48916bda20
ChampionOnyx <shisien_0928@live.com>
2014-02-10 23:02:58 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -136,7 +136,7 @@ exports.herokuhack = false;
 // displayed in the client.
 exports.customavatars = {
 	//'userid': 'customavatar.png'
-	  'champinonyxe': '026.png',
+	  'champinnyxe': '026.png',
 	  'frontierheadbart': '002.gif',
 	  'champinnah': '003.gif',
 	  'artistejeratt': '004.gif',
