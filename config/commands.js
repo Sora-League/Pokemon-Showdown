b21d119e8aabce36c2da4a10a959f24e97a42383
ChampionOnyx <shisien_0928@live.com>
2014-02-28 20:53:09 +0800

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1088,7 +1088,7 @@ var commands = exports.commands = {
 	zoro: function(target, room, user) {
 	        if (!this.canBroadcast()) return;
 		this.sendReplyBox('Gym Ldr <b>Zoro</b><br />'+
-			  '<i>"Your chances of winning is as rare as find a blue mew....well, unless you hack in a shiny mew..."</i> <br />'+
+			  '<i>"Your chances of winning is as rare as finding a blue mew....well, unless you hack in a shiny mew..."</i> <br />'+
 			  '<b>Type:</b> Steel<br />'+
 			  '<b>Ace:</b> Scizor (Quagsire)<br />'+
 			  '<img src="http://sprites.pokecheck.org/i/645.gif"> <img src="http://sprites.pokecheck.org/t/187.gif">');
