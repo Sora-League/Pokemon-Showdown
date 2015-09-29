0b41d26cb919400bd99d52315fb2cdba3a780217
Siiilver <brahadeeshk@gmail.com>
2015-09-29 22:36:49 +0530

    Moved some original commands from custom-commands

--- custom/override.js
+++ custom/override.js
@@ -317,5 +317,122 @@ exports.commands = {
 			spamroom.update();
 		} else targetUser.lastPM = user.userid;
 		user.lastPM = targetUser.userid;
+	},
+
+	uptime: function (target, room, user) {
+		if (!this.canBroadcast()) return;
+		var uptime = process.uptime();
+		var uptimeText;
+		var getUptime = function (uptime) {
+			if (uptime < 24 * 60 * 60) return uptime.seconds().duration();
+			var uptimeDays = Math.floor(uptime / (24 * 60 * 60));
+			var uptimeText = uptimeDays + " " + (uptimeDays === 1 ? "day" : "days");
+			var uptimeHours = Math.floor(uptime / (60 * 60)) - uptimeDays * 24;
+			if (uptimeHours) uptimeText += ", " + uptimeHours + " " + (uptimeHours === 1 ? "hour" : "hours");
+			return uptimeText;
+		}
+		var maxUptime = 'storage-files/maxuptime.txt';
+		if (fs.existsSync(maxUptime)) maxUptime = parseFloat(fs.readFileSync(maxUptime)) || 0;
+		else maxUptime = 0;
+		this.sendReplyBox("Uptime: <b>" + getUptime(uptime) + "</b><br>" + 
+			'<font color = "green">Record Uptime: <b>' + getUptime(maxUptime));
+	},
+
+	kill: function (target, room, user) {
+		if (!this.can('lockdown')) return false;
+
+		if (Rooms.global.lockdown !== true) {
+			return this.sendReply("For safety reasons, /kill can only be used during lockdown.");
+		}
+
+		if (CommandParser.updateServerLock) {
+			return this.sendReply("Wait for /updateserver to finish before using /kill.");
+		}
+
+		for (var i in Users.users) {
+			var shutdownTime = Date.now();
+			Core.write('lastseen', Users.users[i].userid, shutdownTime);
+		}
+		
+		if (!fs.existsSync('storage-files/maxuptime.txt')) fs.writeFileSync('storage-files/maxuptime.txt', process.uptime());
+		else if (parseFloat(fs.readFileSync('storage-files/maxuptime.txt')) < process.uptime()) {
+			fs.writeFileSync('storage-files/maxuptime.txt', process.uptime());
+		}
+
+		for (var i in Sockets.workers) {
+			Sockets.workers[i].kill();
+		}
+
+		if (!room.destroyLog) {
+			process.exit();
+			return;
+		}
+		room.destroyLog(function () {
+			room.logEntry(user.name + " used /kill");
+		}, function () {
+			process.exit();
+		});
+
+		setTimeout(function () {
+			process.exit();
+		}, 10000);
+	},
+
+	join: function (target, room, user, connection) {
+		if (!target) return false;
+		var targetRoom = Rooms.get(target) || Rooms.get(toId(target));
+		if (!targetRoom) {
+			return connection.sendTo(target, "|noinit|nonexistent|The room '" + target + "' does not exist.");
+		}
+		if (targetRoom.isPrivate && !user.named) {
+			return connection.sendTo(target, "|noinit|namerequired|You must have a name in order to join the room '" + target + "'.");
+		}
+		if (!user.joinRoom(targetRoom || room, connection)) {
+			return connection.sendTo(target, "|noinit|joinfailed|The room '" + target + "' could not be joined.");
+		}
+		//If you need to add another IP or host, add a comma after the last element of the array below, and enter
+		//the IP/Host in single or double quotes. Adding a portion of an IP would be acceptable too, since this
+		//filter checks if a user's IP or host contains any of the blacklisted IPs or hosts, rather than
+		//looking for an exact match
+		var blacklist = ["dhcp-077-250-225-247.chello.nl", "c-76-100-209-92.hsd1.md.comcast.net", "zenmate",
+			"74.88.1.127", "27.122.15.28", "mx-ll-223.205.20-59.dynamic.3bb.co.th", "50-108-108-125.adr01.mskg.mi.frontiernet.net",
+			"cpe-67-253-120-124.maine.res.rr.com", "62.140.132.94", "62.140.132.19", "50.84.151.157", "67.164.32.244",
+			"117.216.41.194", "CPE-155-143-4-109.vic.bigpond.net.au", "94.254.0.55", "108.61.179.200.vultr.com",
+			"ool-4573a317.dyn.optonline.net", "69.115.163.23", "173.30.53.93", "71.41.165.94",
+			"bb116-15-8-217.singnet.com.sg", "62.140.132.13", "103-10-199-146.pacswitch.com", "116.14.185.162"
+		];
+		for (var i = 0; i < blacklist.length; i++) {
+			if (user.latestHost.indexOf(blacklist[i]) > -1 || user.latestIp.indexOf(blacklist[i]) > -1) {
+				user.popup('You are on the Sora League banlist or are using a Proxy. GET REKT SON.');
+				user.ban();
+			}
+		}
+	},
+
+	chall: 'challenge',
+	challenge: function (target, room, user, connection) {
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+		if (!targetUser || !targetUser.connected) {
+			return this.popupReply("The user '" + this.targetUsername + "' was not found.");
+		}
+		if (targetUser.blockChallenges && !user.can('bypassblocks', targetUser)) {
+			return this.popupReply("The user '" + this.targetUsername + "' is not accepting challenges right now.");
+		}
+		if (Config.pmmodchat) {
+			var userGroup = user.group;
+			if (Config.groupsranking.indexOf(userGroup) < Config.groupsranking.indexOf(Config.pmmodchat)) {
+				var groupName = Config.groups[Config.pmmodchat].name || Config.pmmodchat;
+				this.popupReply("Because moderated chat is set, you must be of rank " + groupName + " or higher to challenge users.");
+				return false;
+			}
+		}
+		if (toId(target) == 'leaguebattle') {
+			if (!user.can('warn') && !targetUser.can('lock')) return this.popupReply('Only Gym Leaders or higher can be challenged in this format.');
+			else if (user.can('warn') && targetUser.can('lock')) return this.popupReply('Only challengers can be challenged in this format.');
+		}
+		user.prepBattle(target, 'challenge', connection, function (result) {
+			if (result) user.makeChallenge(targetUser, target);
+		});
 	}
 };
