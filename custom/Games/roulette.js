a707b26ca76f1630dbbc5f4e4c0a44905dd00bca
Siiilver <brahadeeshk@gmail.com>
2015-10-04 01:34:27 +0530

    Edited a bit

--- custom/Games/roulette.js
+++ custom/Games/roulette.js
@@ -1,21 +1,20 @@
-const COLORS = {'red':1, 'blue':1, 'yellow':1, 'green':1, 'black':1};
-
+const EXPIRATION_TIME = 1000 * 60;
 var roulettes = exports.roulettes = {};
+var colors = {'red': 3, 'blue': 3, 'yellow': 4, 'green': 4, 'black': 7};
 
 var Roulette = (function () {
-	function Roulette (room) {
+	function Roulette(room) {
 		this.room = room;
 		this.players = {};
-		var that = this;
 		this.timer = setTimeout(function () {
-			if (Object.keys(that.players).length < 1) {
-				that.room.add('|raw|<b>The roulette has been ended due to the lack of players');
-				delete roulettes[that.room.id];
+			if (Object.keys(this.players).length < 1) {
+				this.room.add('|raw|<b>The roulette has been ended due to the lack of players');
+				delete roulettes[this.room.id];
 				return;
 			}
 			that.spin();
 			that.room.update();
-		}, 1000 * 60); //1 minute
+		}.bind(this), EXPIRATION_TIME);
 	}
 	Roulette.prototype.placeBet = function (user, color, self) {
 		for (var i = 0; i < user.getAlts().length; i++) {
@@ -36,7 +35,7 @@ var Roulette = (function () {
 		} else {
 			if (this.players[user.userid].color !== color) {
 				var bets = (this.players[user.userid].bets === 1 ? 'bet' : 'bets');
-				self.sendReply('You are now betting on ' + color +' instead of ' + this.players[user.userid].color + '. You are currently placing ' + this.players[user.userid].bets + ' '+ bets + ' on ' + color + '.');
+				self.sendReply('You are now betting on ' + color + ' instead of ' + this.players[user.userid].color + '. You are currently placing ' + this.players[user.userid].bets + ' ' + bets + ' on ' + color + '.');
 				this.players[user.userid].color = color;
 			} else {
 				this.players[user.userid].bets++;
@@ -61,54 +60,41 @@ var Roulette = (function () {
 	};
 	Roulette.prototype.spin = function () {
 		var random = Math.floor((Math.random() * 11) + 1);
-        var payout;
-        var color;
+		var payout;
+		var color;
 		var winners = [];
-        if (random <= 3) {
-            color = 'red';
-            payout = 3;
-        } else if (random <= 6) {
-            color = 'yellow';
-            payout = 3;
-        } else if (random <= 8) {
-            color = 'blue';
-            payout = 4;
-        } else if (random <= 10) {
-            color = 'green';
-            payout = 4;
-        } else if (random <= 11) {
-            color = 'black';
-            payout = 7;
-        } else {
-            color = 'red';
-			payout = 3;
-        }
-	
+		if (random <= 3) color = 'red';
+		else if (random <= 6) color = 'yellow';
+		else if (random <= 8) color = 'blue';
+		else if (random <= 10) color = 'green';
+		else color = 'black';
+		payout = colors[color];
+
 		for (var i in this.players) {
-            if (this.players[i].color === color) winners.push(i);
-        }
-		
-        if (winners.length <= 0) { 
-            this.room.add('|html|<div class = "infobox"><font size = 2, color = "green"><center><b>The roulette has been spun!</font><br />' +
-                '<center>The roulette landed on <font color = "' + color + '"><b>' + color + '<b>!</font><br />' +
-                '<center>But nobody won this time...');
-        } else {
-            var winlist = '';
-            for (var i = 0; i < winners.length; i++) {
+			if (this.players[i].color === color) winners.push(i);
+		}
+
+		if (winners.length <= 0) {
+			this.room.add('|html|<div class = "infobox"><font size = 2, color = "green"><center><b>The roulette has been spun!</font><br />' +
+				'<center>The roulette landed on <font color = "' + color + '"><b>' + color + '<b>!</font><br />' +
+				'<center>But nobody won this time...');
+		} else {
+			var winlist = '';
+			for (var i = 0; i < winners.length; i++) {
 				if (i > 0) winlist += ', ';
-                var winamount = this.players[winners[i]].bets * payout;
+				var winamount = this.players[winners[i]].bets * payout;
 				var name = (Users.getExact(winners[i])) ? Users.getExact(winners[i]).name : winners[i];
-                winlist += '<b>' + name + '</b> who won <b>' + winamount + '</b> points';
-            }
-            if (winners.length == 1) {
+				winlist += '<b>' + name + '</b> who won <b>' + winamount + '</b> points';
+			}
+			if (winners.length == 1) {
 				this.room.add('|html|<div class = "infobox"><font size = 2, color = "green"><center><b>The roulette has been spun!</font><br />' +
-                '<center>The roulette landed on <font color = "' + color + '"><b>' + color + '<b>!</font><br />' +
-                '<center>The only winner is ' + winlist);
+					'<center>The roulette landed on <font color = "' + color + '"><b>' + color + '<b>!</font><br />' +
+					'<center>The only winner is ' + winlist);
 			} else {
 				this.room.add('|html|<div class = "infobox"><font size = 2, color = "green"><center><b>The roulette has been spun!</font><br />' +
-                '<center>The roulette landed on <font color = "' + color + '"><b>' + color + '<b>!</font><br />' +
-                '<center>The winners are:<br/>' +
-                '<center>' + winlist);
+					'<center>The roulette landed on <font color = "' + color + '"><b>' + color + '<b>!</font><br />' +
+					'<center>The winners are:<br/>' +
+					'<center>' + winlist);
 			}
 			winners.forEach(function (user) {
 				user = Users.get(user);
@@ -125,63 +111,63 @@ var cmds = {
 	players: 'participants',
 	participants: function (target, room, user) {
 		if (!roulettes[room.id]) return this.sendReply('There is no roulette going on in this room right now.');
-		this.sendReplyBox('Number of Roulette players: ' + Object.keys(roulettes[room.id].players));
+		this.sendReplyBox('Number of Roulette players: ' + Object.keys(roulettes[room.id].players).length);
 	},
-	
+
 	help: 'rules',
 	'': 'rules',
 	commands: 'rules',
 	rules: function (target, room, user) {
 		if (!this.canBroadcast()) return;
 		return this.sendReplyBox('<b><center>Roulette rules and commands</center></font></b><br />' +
-            '-/startroul or /roul start - Starts a roulette game in the room. Must be ranked + or higher to use.<br />' +
-            '-/bet or /roul bet <i>Color</i> - Bets on a roulette color. Using this multiple times increases the number of times you\'ve bet on that color by 1. You require 1 buck per bet. Clicking on a different color changes the color you\'re betting on to the new color. <br />' +
-            '-/participants or /roul players - Shows the number of participants in the game.<br />' +
-            '-/deletebets or /roul deletebets - Erases all of the bets you have made so far. Any bucks spent on bets will be refunded.<br />' +
-            '-/spin or /roul spin - Spins the roulette. Must be ranked + or higher to use.<br />' +
-            '-/endroul or /roul end - Ends the game of roulette in the room. Any bets made will be refunded. Must be ranked + or higher to use.<br />' +
-            'The values of red and yellow are 4, blue and green 5, and black, 8. The cash reward is equal to the color\'s prize value multiplied by the number of bets you\'ve made, if the roulette lands on the color you\'ve bet on.');
+			'-/startroul or /roul start - Starts a roulette game in the room. Must be ranked + or higher to use.<br />' +
+			'-/bet or /roul bet <i>Color</i> - Bets on a roulette color. Using this multiple times increases the number of times you\'ve bet on that color by 1. You require 1 buck per bet. Clicking on a different color changes the color you\'re betting on to the new color. <br />' +
+			'-/participants or /roul players - Shows the number of participants in the game.<br />' +
+			'-/deletebets or /roul deletebets - Erases all of the bets you have made so far. Any bucks spent on bets will be refunded.<br />' +
+			'-/spin or /roul spin - Spins the roulette. Must be ranked + or higher to use.<br />' +
+			'-/endroul or /roul end - Ends the game of roulette in the room. Any bets made will be refunded. Must be ranked + or higher to use.<br />' +
+			'The values of red and yellow are 4, blue and green 5, and black, 8. The cash reward is equal to the color\'s prize value multiplied by the number of bets you\'ve made, if the roulette lands on the color you\'ve bet on.');
 	},
-	
+
 	'new': 'start',
 	start: function (target, room, user) {
 		if (!this.can('broadcast', null, room)) return false;
 		if (roulettes[room.id]) return this.sendReply('There is already a game of roulette going on.');
 		roulettes[room.id] = new Roulette(room);
 		this.add('|html|<div class = "infobox"><font size = 3, color = "green"><center><b>' + user.name + ' has started a roulette!</font><br />' +
-            '<center><button name = "send", value = "/bet red"><font color = "red"><b>Red</b></font><button name = "send", value = "/bet yellow"><font color = "yellow"><b>Yellow</b></font></button><button name = "send", value = "/bet blue"><font color = "blue"><b>Blue</b></font></button><button name = "send", value = "/bet green"><font color = "green"><b>Green</b></font><button name = "send", value = "/bet black"><font color = "black"><b>Black</b></font></button>' +
-            '<font size = 1><center>Click one of the buttons or use /bet [color] to place a bet on a color!</font><br />' +
-            '<font size = 1><center>Enter /roul in the chat for a list of roulette commands and rules.</font><br /></div>');
+			'<center><button name = "send", value = "/bet red"><font color = "red"><b>Red</b></font><button name = "send", value = "/bet yellow"><font color = "yellow"><b>Yellow</b></font></button><button name = "send", value = "/bet blue"><font color = "blue"><b>Blue</b></font></button><button name = "send", value = "/bet green"><font color = "green"><b>Green</b></font><button name = "send", value = "/bet black"><font color = "black"><b>Black</b></font></button>' +
+			'<font size = 1><center>Click one of the buttons or use /bet [color] to place a bet on a color!</font><br />' +
+			'<font size = 1><center>Enter /roul in the chat for a list of roulette commands and rules.</font><br /></div>');
 	},
-	
+
 	bet: function (target, room, user) {
 		if (!roulettes[room.id]) return this.sendReply('There is no roulette going on in this room right now.');
 		if (Core.read('money', user.userid) < 1) return this.sendReply("You don't have enough money to place bets.");
-        target = toId(target);
-        if (!COLORS[target]) return this.sendReply(target + ' is not a valid color');
-		
+		target = toId(target);
+		if (!colors[target]) return this.sendReply(target + ' is not a valid color');
+
 		roulettes[room.id].placeBet(user, target, this);
 	},
-	
+
 	deletebets: 'delete',
 	'delete': function (target, room, user) {
 		if (!roulettes[room.id]) return this.sendReply('There is no roulette going on in this room right now.');
-		
+
 		roulettes[room.id].deleteBets(user, this);
 	},
-	
-	end: 'stop', 
+
+	end: 'stop',
 	stop: function (target, room, user) {
 		if (!this.can('broadcast', null, room)) return false;
 		if (!roulettes[room.id]) return this.sendReply('There is no roulette going on in this room right now.');
-		
+
 		roulettes[room.id].end(user);
 	},
-	
+
 	spin: function (target, room, user) {
 		if (!this.can('broadcast', null, room)) return false;
 		if (!roulettes[room.id]) return this.sendReply('There is no roulette going on in this room right now.');
-		
+
 		roulettes[room.id].spin();
 	}
 };
@@ -191,10 +177,10 @@ exports.commands = {
 	roulette: cmds,
 	players: 'participants',
 	participants: cmds.participants,
-	roulrules: cmds.rules,
+	roulrules: 'roulhelp',
 	roulhelp: cmds.rules,
 	startroul: cmds.start,
-	roulend: cmds.stop,
+	roulend: 'endroul',
 	endroul: cmds.stop,
 	deletebets: cmds['delete'],
 	bet: cmds.bet,
