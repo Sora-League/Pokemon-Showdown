ef3a1df72e27a88f7235274641bf0ae7c9b1ca91
Blazing360 <Rotflol360@gmail.com>
2015-11-07 15:53:02 +1100

    Create backgrounds.js

---
+++ custom/backgrounds.js
@@ -0,0 +1,9 @@
+exports.commands = {
+
+       backgrounds: 'background',
+       background: function(target, room, user) {
+		         if (!this.canBroadcast()) return;
+		         this.sendReplyBox('test');}
+		         
+
+};
