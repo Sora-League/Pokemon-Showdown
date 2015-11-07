02ae5eb20a7147300d8b015288171e7f6620ba72
Blazing360 <Rotflol360@gmail.com>
2015-11-07 15:56:37 +1100

    Update backgrounds.js

--- custom/backgrounds.js
+++ custom/backgrounds.js
@@ -4,8 +4,8 @@ backgrounds: 'background',
 background: function(target, room, user) {
         if (!this.canBroadcast()) return;
         this.sendReplyBox('<table style="width:100%"><tr>'+
-                '<td><a href="https://www.dropbox.com/s/l0g3wm01jcdewa0/Arani%27s%20Dedication.png?dl=1"><img src="https://www.dropbox.com/s/l0g3wm01jcdewa0/Arani%27s%20Dedication.png?raw=1" width=170></a></td>'
-                '<td><a href="https://www.dropbox.com/s/3jpqyt76wch91la/Jeratt%27s%20Team.png?dl=1"><img src="https://www.dropbox.com/s/3jpqyt76wch91la/Jeratt%27s%20Team.png?raw=1" width=170></a></td>'
+                '<td><a href="https://www.dropbox.com/s/l0g3wm01jcdewa0/Arani%27s%20Dedication.png?dl=1"><img src="https://www.dropbox.com/s/l0g3wm01jcdewa0/Arani%27s%20Dedication.png?raw=1" width=170></a></td>'+
+                '<td><a href="https://www.dropbox.com/s/3jpqyt76wch91la/Jeratt%27s%20Team.png?dl=1"><img src="https://www.dropbox.com/s/3jpqyt76wch91la/Jeratt%27s%20Team.png?raw=1" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/fk3ypvjduwqnls5/Noah%27s%20Water%20Mono.png?dl=1"><img src="https://www.dropbox.com/s/fk3ypvjduwqnls5/Noah%27s%20Water%20Mono.png?raw=1" width=170></a></td>');
 	}
 		         
