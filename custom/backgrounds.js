027d739585e46f6e9c3e588f73695a9f6f746014
Blazing360 <Rotflol360@gmail.com>
2015-11-24 23:17:01 +1100

    Update backgrounds.js

--- custom/backgrounds.js
+++ custom/backgrounds.js
@@ -20,13 +20,13 @@ background: function(target, room, user) {
                 '<details><summary>Jeratt\'s 2014 Collection (Click me to expand)</summary><table style="width:100%"><tr>'+
                 '<td><a href="https://www.dropbox.com/s/82xvmq22aoeas3b/Bug%20Type.png?dl=1"><img src="http://i.imgur.com/SaepSCp.png" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/p8tcozimsnwpizz/Dark%20Type.png?dl=1"><img src="http://i.imgur.com/T7C8R0G.png" width=170></a></td>'+
-                '<td><a href="https://www.dropbox.com/s/hg4fejrghcpq0k2/Dragon%20Type.png?dl=1"><img src="http://i.imgur.com/PhmyjpU.png" width=170></a></td>'+
+                '<td><a href="https://www.dropbox.com/s/hg4fejrghcpq0k2/Dragon%20Type.png?dl=1"><img src="http://i.imgur.com/PhmyjpU.png" width=170></a></td></tr>'+
                 
-                '<td><a href="https://www.dropbox.com/s/9gb3bl7j8g7sdyq/Electric%20Type.png?dl=1"><img src="http://i.imgur.com/9nY2thJ.png" width=170></a></td>'+
+                '<tr><td><a href="https://www.dropbox.com/s/9gb3bl7j8g7sdyq/Electric%20Type.png?dl=1"><img src="http://i.imgur.com/9nY2thJ.png" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/b0tgqyr90jowdjf/Ghost%20Type.png?dl=1"><img src="http://i.imgur.com/OV7y9Xz.png" width=170></a></td>'+
-                '<td><a href="https://www.dropbox.com/s/wj1ml2mfwkrfw1c/Jeratt%27s%20Dedication.png?dl=1"><img src="http://i.imgur.com/u93VDak.png" width=170></a></td>'+
+                '<td><a href="https://www.dropbox.com/s/wj1ml2mfwkrfw1c/Jeratt%27s%20Dedication.png?dl=1"><img src="http://i.imgur.com/u93VDak.png" width=170></a></td></tr>'+
                 
-                '<td><a href="https://www.dropbox.com/s/wpu4g3qu388ipdc/Onyx%27s%20Dedication.png?dl=1"><img src="http://i.imgur.com/g4GGZXw.png" width=170></a></td>'+
+                '<tr><td><a href="https://www.dropbox.com/s/wpu4g3qu388ipdc/Onyx%27s%20Dedication.png?dl=1"><img src="http://i.imgur.com/g4GGZXw.png" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/kf3me6ebcaidaws/Sora%27s%20X-mas.png?dl=1"><img src="http://i.imgur.com/h3MxvAr.png" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/gijxahuc894odcq/Toast%27s%20Fire%20Mono.png?dl=1"><img src="http://i.imgur.com/Cc6FJwG.png" width=170></a></td></tr></table><br>'+
                 '<font size=0.5>These images belong to <b>Artiste Jeratt</b>, citing is required before reusage.</font></details><br><br>'
