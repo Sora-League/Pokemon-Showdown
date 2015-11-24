0c23e48bcba0fde03e270e687f3aba2170e1b266
Blazing360 <Rotflol360@gmail.com>
2015-11-24 22:39:10 +1100

    Update backgrounds.js

--- custom/backgrounds.js
+++ custom/backgrounds.js
@@ -5,7 +5,7 @@ background: function(target, room, user) {
         if (!this.canBroadcast()) return;
         this.sendReplyBox('<center><text size=3><b>Sora Backgrounds</b></text><br>This is a list of past and current server backgrounds made available for you to download and use as a desktop backgrounds.<br>Simply browse through the images and click on the ones you would like to download, accept the external website link and the download will start automatically.<br>'
         	
-        	'<details><summary>Jeratt\'s 2013 Collection (Click me to expand)</summary><table style="width:100%"><tr>'+
+        	'<details><summary>Jeratt\'s 2013 Collection \(Click me to expand\)</summary><table style="width:100%"><tr>'+
                 '<td><a href="https://www.dropbox.com/s/l0g3wm01jcdewa0/Arani%27s%20Dedication.png?dl=1"><img src="http://i.imgur.com/B0J62kf.png" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/3jpqyt76wch91la/Jeratt%27s%20Team.png?dl=1"><img src="http://i.imgur.com/yINqS9w.png" width=170></a></td>'+
                 '<td><a href="https://www.dropbox.com/s/fk3ypvjduwqnls5/Noah%27s%20Water%20Mono.png?dl=1"><img src="http://i.imgur.com/yrzodOo.png" width=170></a></td></tr>'+
