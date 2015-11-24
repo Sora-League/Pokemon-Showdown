75ea8e493f1e7fc5c672db30d7821a38532a17bb
Blazing360 <Rotflol360@gmail.com>
2015-11-24 22:45:30 +1100

    Update backgrounds.js

--- custom/backgrounds.js
+++ custom/backgrounds.js
@@ -3,7 +3,7 @@ exports.commands = {
 backgrounds: 'background',
 background: function(target, room, user) {
         if (!this.canBroadcast()) return;
-        this.sendReplyBox('<center><text size=3><b>Sora Backgrounds</b></text><br>This is a list of past and current server backgrounds made available for you to download and use as a desktop backgrounds.<br>Simply browse through the images and click on the ones you would like to download, accept the external website link and the download will start automatically.<br>'+
+        this.sendReplyBox('<center><font size=3><b>Sora Backgrounds</b></font><br>This is a list of past and current server backgrounds made available for you to download and use as a desktop backgrounds.<br><br>Simply browse through the images and click on the ones you would like to download, accept the external website link and the download will start automatically.<br><br><br>'+
         	
         	'<details><summary>Jeratt\'s 2013 Collection (Click me to expand)</summary><br><table style="width:100%"><tr>'+
                 '<td><a href="https://www.dropbox.com/s/l0g3wm01jcdewa0/Arani%27s%20Dedication.png?dl=1"><img src="http://i.imgur.com/B0J62kf.png" width=170></a></td>'+
@@ -15,7 +15,7 @@ background: function(target, room, user) {
                 '<td><a href="https://www.dropbox.com/s/r79nb8rwzvflx34/Poison%20Type.png?dl=1"><img src="http://i.imgur.com/iBMDE6g.png" width=170></a></td></tr>'+
                 
                 '<tr><td><a href="https://www.dropbox.com/s/jr38uagsk6wyksb/Risu%27s%20Dragon%20Mono.png?dl=1"><img src="http://i.imgur.com/g4QeSP8.png" width=170></a></td>'+
-                '<td>These images belong to Artiste Jeratt, citing required before reusage.</tr></details>'
+                '<td></center>These images belong to <b>Artiste Jeratt</b>, citing is required before reusage.</tr></details>'
                 );
 	}
 		         
