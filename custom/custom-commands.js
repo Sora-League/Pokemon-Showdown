99368605042f101a830c87a98ba755bed5bfe746
Siiilver <brahadeeshk@gmail.com>
2015-12-01 19:23:51 +0530

    Update custom-commands.js

--- custom/custom-commands.js
+++ custom/custom-commands.js
@@ -67,6 +67,7 @@ exports.commands = {
 				return this.sendReply('WARNING: This will permanently delete all tournament ladder ratings. If you\'re sure you want to do this, use this command again.');
 			}
 			deleteLadderConfirm = false;
+			delete Ladders.ladderCaches['tournaments'];
 			require('fs').unlinkSync('config/ladders/tournaments.tsv');
 			Rooms('lobby').add('|html|<b>The Tournament Ladder has been reset.</b>');
 			Rooms('lobby').update();
