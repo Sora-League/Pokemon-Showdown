a33486ce8253a5d4301d1146cf4e00012fa66865
Silveee <brahadeeshk@gmail.com>
2016-08-13 13:15:34 +0530

    Add Sora Jeratt to A-team, remove Sora Jerattata

--- chat-plugins/modcommands.js
+++ chat-plugins/modcommands.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const request = require('request');
 
 let ateam = {'femalegallade':1, 'sorarevan':1, 'blazing360': 1, 'sorablade': 1,
-	'onyxeagle':1, 'soraonyxeagle':1, 'jeratt':1, 'sorajerattata':1, 'neithcass':1, 'sorabarts': 1,
+	'onyxeagle':1, 'soraonyxeagle':1, 'jeratt':1, 'sorajeratt':1, 'neithcass':1, 'sorabarts': 1,
 	'soraninjarisu':1, 'soraneith': 1
 };
 
