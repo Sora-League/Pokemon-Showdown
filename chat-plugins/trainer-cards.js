ebf3fc9bd57832c0af8e395944cd7b0aab7a04e7
Loln8or <ancestraldog@hotmail.com>
2016-10-22 21:22:37 +1100

    Update trainer-cards.js

--- chat-plugins/trainer-cards.js
+++ chat-plugins/trainer-cards.js
@@ -349,10 +349,10 @@ exports.commands = {
 	
 	flying: function (target, room, user) {
 		if (!this.runBroadcast()) return;
-		this.sendReplyBox('∆Gym Ldr <b>???</b>∆<br />' +
-			'<i>"???"</i> <br />' +
+		this.sendReplyBox('∆Gym Ldr <b>Youmaton</b>∆<br />' +
+			'This member is a part of Sora\'s inactivity clause, and is a member of both the Frontier AND the Gym Leaders. This type is still open for challengers who wish to apply for it. <br />' +
 			'<b>Type: <font color = 7ab6ff>Flying</font></b><br />' +
-			'<b>Ace:</b> ???<br />' +
+			'<b>Ace:</b> Togekiss<br />' +
 			seen('') + getBadges(''));
 	},
 
