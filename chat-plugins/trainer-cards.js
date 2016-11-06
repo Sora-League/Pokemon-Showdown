136cac837e50bbed69785b3864f5531a0013e60e
Loln8or <ancestraldog@hotmail.com>
2016-11-07 00:58:55 +1100

    Update trainer-cards.js

--- chat-plugins/trainer-cards.js
+++ chat-plugins/trainer-cards.js
@@ -146,8 +146,8 @@ exports.commands = {
 		if (!this.runBroadcast()) return;
 		this.sendReplyBox('∆E4 <b>Meows</b>∆<br />' +
 			'<i>"1et m3 b3 th3 k3y 2 ur h3art xoxo"</i> <br />' +
-			'<b>Type: <font color = 0745ff>Water</font></b><br />' +
-			'<b>Ace:</b> Volcanion<br />' + seen('sorafloat') + getBadges('sorafloat'));
+			'<b>Type: <font color = ff42a0>Fairy</font></b><br />' +
+			'<b>Ace:</b> Klefki<br />' + seen('sorafloat') + getBadges('sorafloat'));
 
 	},
 	
