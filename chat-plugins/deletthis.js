372cf53b302d83c9c8fc4821549d244d3b0e0778
Blazing360 <Rotflol360@gmail.com>
2016-12-01 01:54:38 +1100

    Created a new command!

---
+++ chat-plugins/deletthis.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const FILE = 'storage-files/delet.json';
+const fs = require('fs');
+let deletoff = false
+let delets = JSON.parse(fs.readFileSync(FILE));
+
+function saveDelets() {
+	fs.writeFileSync(File, JSON.stringify(delets, null, 1));
+}
+
+exports.commands = {
+	
+deletthis: function (target, room, user) {
+			if (!this.canTalk()) return this.errorReply("You cannot use this command");
+			if (deletoff) return this.sendReply("'Delet This' are currently disabled.");
+		let message = delets[Math.floor(Math.random() * delets.length)];
+		this.add('|html|<center><b><img src=" ' + message + ' "></b>');
+	},
+	
+};
