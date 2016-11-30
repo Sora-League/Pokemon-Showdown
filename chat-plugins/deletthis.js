94a4e3f2dccaf8d40e48528ff7f1cf0fb49d48eb
Blazing360 <Rotflol360@gmail.com>
2016-12-01 02:07:46 +1100

    lets see if this works

--- chat-plugins/deletthis.js
+++ chat-plugins/deletthis.js
@@ -15,7 +15,7 @@ deletthis: function (target, room, user) {
 			if (!this.canTalk()) return this.errorReply("You cannot use this command");
 			if (deletoff) return this.sendReply("'Delet This' are currently disabled.");
 		let message = delets[Math.floor(Math.random() * delets.length)];
-		this.add('|html|<center><b><img src=" ' + message + ' "></b>');
+		this.sendReplyBox('<img src="' + message + '">');
 	},
 	
 };
