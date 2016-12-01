05eb1b1f54c894180f2068d340ee9d4c61e0541c
Blazing360 <Rotflol360@gmail.com>
2016-12-02 01:38:15 +1100

    lets see if this works

--- chat-plugins/misc.js
+++ chat-plugins/misc.js
@@ -123,8 +123,8 @@ exports.commands = {
 	u: 'urbandefine',
 	ud: 'urbandefine',
 	urbandefine: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-		if (room.id === 'lobby' && this.broadcasting && this.can('hotpatch')) return this.sendReply('You cannot broadcast this command in the lobby.');
+		if (!this.runBroadcast() && this.can('hotpatch')) return;
+		else if (room.id === 'lobby' && this.broadcasting) return this.sendReply('You cannot broadcast this command in the lobby.');
 		if (!target) return this.parse('/help urbandefine')
 		if (target > 50) return this.sendReply('Phrase can not be longer than 50 characters.');
 
